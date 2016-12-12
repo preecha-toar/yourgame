c48698aaefc9d62fc39c8d35f716e365d4f51db1
toar <exzise@gmail.com>
2016-12-13 00:55:09 +0700

    frist commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+var mainState = {
+    preload: function() {
+    	game.load.image('devahoy','logo.png');
+    },
+
+    create: function() {
+    	game.stage.backgroundColor = '#e26a6b'
+    	game.add.sprite(0,0,'devahoy');
+    },
+
+    update: function() {
+
+    }
+};
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+
+game.state.add('main', mainState);
+game.state.start('main');
