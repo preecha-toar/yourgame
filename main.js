c12d634b19630604e288cb73742f90029659107b
toar <exzise@gmail.com>
2016-12-13 13:48:10 +0700

    new update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var mainState = {
     preload: function() {
-    	game.load.image('devahoy','logo.png');
+    	game.load.image('devahoy','assets/logo.png');
     },
 
     create: function() {
-    	game.stage.backgroundColor = '#e26a6b'
+    	game.stage.backgroundColor = '#a8e8ff';
     	game.add.sprite(0,0,'devahoy');
     },
 
@@ -12,8 +12,6 @@ var mainState = {
 
     }
 };
-
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
-
 game.state.add('main', mainState);
 game.state.start('main');
