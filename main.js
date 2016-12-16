9a990bf03d730ef14a7a65049eb9fd9754ead3bc
toar <exzise@gmail.com>
2016-12-16 23:58:49 +0700

    input key

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ var mainState = {
 
     preload: function() {
     	game.load.spritesheet('player', 'assets/character.png', 65, 85);
-    	game.load.spritesheet('shot', 'assets/shot.png', 30,18);
+    	game.load.spritesheet('bullet', 'assets/shot.png', 30,18);
 
     },
 
     create: function() {
-    	var keyA;
     	game.physics.startSystem(Phaser.Physics.ARCADE);
     	game.stage.backgroundColor = '#a8e8ff';
 		this.myWorld = game.add.group();
     	this.myWorld.enableBody = true;
 
-    	
+    	bullets = game.add.group();
+    	bullets.createMultiple(500, 'bullet', 0, false);
     	
     	//player
 		this.player = game.add.sprite(0, 450, 'player');
 		game.physics.arcade.enable(this.player);
+		//game.camera.follow(player);
 		//this.player.body.bounce.y = 0.25;
 		//this.player.body.gravity.y = 980;
 		this.player.body.collideWorldBounds = true;
@@ -28,10 +29,11 @@ var mainState = {
 		this.player.frame = 1;
 
 		this.corsors = this.input.keyboard.createCursorKeys();
-		//***************************************************
+	
 		keyA = game.input.keyboard.addKey(Phaser.Keyboard.A);
-		keyA.onDown.add(addPhaserShot, this);
-		//game.input.keyboard.removeKeyCapture(Phaser.Keyboard.a);
+		keyA.onDown.add(this.shot_now, this);
+		game.input.keyboard.removeKeyCapture(Phaser.Keyboard.a);
+		
     },
 
     update: function() {
@@ -51,14 +53,20 @@ var mainState = {
     		this.player.body.velocity.x = 100;
     		this.player.animations.play('right');
     	}
+    	
+    	
     },
-    /*
-    addPhaserShot: function(){
-    	this.game.add.sprite(game.player.X, game.player.Y, 'shot');
-    	this.shot.body.velocity.x = -200;
-    }*/
+	shot_now: function(){
+    	game.add.sprite(game.world.randomX, game.world.randomY, 'bullet');
+    	//fireball.reset(player.x-20, player.y);
+    }
 };
-
+var shotRate = 300;
+var nextshot = 0;
+var bullets;
+var shot = false;
+var fireRate = 300;
+var keyA;
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 game.state.add('main', mainState);
 game.state.start('main');
