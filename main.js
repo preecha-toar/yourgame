08e4d65224be514169d77d416c2951ed13dc34ab
toar <exzise@gmail.com>
2016-12-27 16:10:18 +0700

    update 12/27/2016

--- main.js
+++ main.js
@@ -3,28 +3,42 @@ var mainState = {
     preload: function() {
     	game.load.spritesheet('player', 'assets/character.png', 65, 85);
     	game.load.spritesheet('bullet', 'assets/shot.png', 20,20);
+    	game.load.spritesheet('box','assets/box.png', 30, 200);
 
     },
 
     create: function() {
     	game.physics.startSystem(Phaser.Physics.ARCADE);
     	game.stage.backgroundColor = '#a8e8ff';
+
 		this.myWorld = game.add.group();
     	this.myWorld.enableBody = true;
+ 
+
+    	box = game.add.physicsGroup(Phaser.Physics.ARCADE);
+    	for(var i = 0; i < 10; i++){
+    		var b = box.create(game.world.randomX, Math.random()*500, 'box', game.rnd.integerInRange(0,36));
+    		b.name = 'bo' + i;
+    		b.body.immovable = true;
+    	}
+    	
+
+    	
+    	//var box = this.myWorld.create(500,150,'box');
+    	//box.body.immovable = true;
 
     	bullets = game.add.group();
     	bullets.enableBody = true;
     	bullets.physicsBodyType = Phaser.Physics.ARCADE;
     	bullets.createMultiple(20, 'bullet', 0, false);
-
     	bullets.setAll('anchor.x', 0.5);
     	bullets.setAll('anchor.y', 1);
     	bullets.setAll('outOfBoundsKill', true);
     	bullets.setAll('checkWorldBounds',true);
-    	keyA = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    	keyA = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		keyA.onDown.add(this.shot_now, this);
-		
 
+	
     	//player
 		this.player = game.add.sprite(0, 450, 'player');
 		game.physics.arcade.enable(this.player);
@@ -41,10 +55,12 @@ var mainState = {
 		this.player.frame = 1;
 
 		this.corsors = this.input.keyboard.createCursorKeys();		
+
     },
 
     update: function() {
-
+    	game.physics.arcade.collide(this.player, box);
+    	game.physics.arcade.overlap(bullets,box,this.collectBullet, null, this);
     	this.player.body.velocity.x = 0;
     	this.player.body.velocity.y = 0;
     	if(this.corsors.up.isDown){
@@ -64,9 +80,7 @@ var mainState = {
     		this.player.animations.play('right');
     		state = 2;
 		}
-    
-    	
-    },
+     },
 	shot_now: function(){
 		if(game.time.now > nextshot){
     		nextshot = game.time.now + shotRate;
@@ -87,8 +101,13 @@ var mainState = {
     			}
     		}
     	}
+   	},
+   	collectBullet: function(bullets){
+   		bullets.kill();
    	}
+
 };
+var box;
 var state;
 var bulletButton;
 var player;
